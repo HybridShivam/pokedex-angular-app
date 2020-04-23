c1bd324a20253f64fa6adfaf8af03ff88330b691
Shivam Baghel <shivamunlimited@gmail.com>
2020-04-23 13:21:59 +0530

    Official Thumbnails Added !!!

--- src/app/pokemon-list/pokemon-item/pokemon-item.component.ts
+++ src/app/pokemon-list/pokemon-item/pokemon-item.component.ts
@@ -10,11 +10,22 @@ export class PokemonItemComponent implements OnInit {
 
 
   @Input() pokemon: Pokemon;
+  pokemonThumbnail;
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.pokemonThumbnail = 'https://raw.githubusercontent.com/HybridShivam/pokemon.json/master/thumbnails/' +
+      this.pad(this.pokemon.id, 3) + '.png';
+  }
+
+  pad(number, length) {
+    let str = '' + number;
+    while (str.length < length) {
+      str = '0' + str;
+    }
+    return str;
   }
 
 }
