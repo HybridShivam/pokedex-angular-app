d42d3de9657a302d937031b4a9b5e3a944d6c776
Shivam Baghel <shivamunlimited@gmail.com>
2020-04-30 12:01:21 +0530

    Update sidebar.component.ts

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -3,6 +3,7 @@ import {trigger, state, style, transition, animate} from '@angular/animations';
 import {SidebarService} from './sidebar.service';
 import {LocationStrategy} from '@angular/common';
 import {ActivatedRoute, Router} from '@angular/router';
+
 // import { MenusService } from './menus.service';
 
 @Component({
@@ -25,6 +26,43 @@ export class SidebarComponent implements OnInit, DoCheck {
   constructor(public sidebarservice: SidebarService, private locationStrategy: LocationStrategy, private router: Router,
               private activatedRoute: ActivatedRoute) {
     this.menus = sidebarservice.getMenuList();
+    // (function (global) {
+    //
+    //   if (typeof (global) === 'undefined') {
+    //     throw new Error('window is undefined');
+    //   }
+    //
+    //   var _hash = '!';
+    //   var noBackPlease = function () {
+    //     global.location.href += '#';
+    //
+    //     // making sure we have the fruit available for juice (^__^)
+    //     global.setTimeout(function () {
+    //       global.location.href += '!';
+    //     }, 50);
+    //   };
+    //
+    //   global.onhashchange = function () {
+    //     if (global.location.hash !== _hash) {
+    //       global.location.hash = _hash;
+    //     }
+    //   };
+    //
+    //   global.onload = function () {
+    //     noBackPlease();
+    //
+    //     // disables backspace on page except on input fields and textarea..
+    //     document.body.onkeydown = function (e) {
+    //       var elm = e.target.nodeName.toLowerCase();
+    //       if (e.which === 8 && (elm !== 'input' && elm !== 'textarea')) {
+    //         e.preventDefault();
+    //       }
+    //       // stopping event bubbling up the DOM tree..
+    //       e.stopPropagation();
+    //     };
+    //   };
+    //
+    // })(window);
   }
 
   ngOnInit() {
