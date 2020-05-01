69f84578f7507de33de23cf5f95c0f4258199e7f
Shivam Baghel <shivamunlimited@gmail.com>
2020-05-01 11:53:27 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
 // import "@angular/compiler";
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import {AppRoutingModule} from './app-routing.module';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 
-import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import {AppComponent} from './app.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
 
-import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
-import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
-import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+import {PerfectScrollbarModule} from 'ngx-perfect-scrollbar';
+import {PERFECT_SCROLLBAR_CONFIG} from 'ngx-perfect-scrollbar';
+import {PerfectScrollbarConfigInterface} from 'ngx-perfect-scrollbar';
 import {HeaderComponent} from './header/header.component';
-import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
-import { PokemonItemComponent } from './pokemon-list/pokemon-item/pokemon-item.component';
+import {PokemonListComponent} from './pokemon-list/pokemon-list.component';
+import {PokemonItemComponent} from './pokemon-list/pokemon-item/pokemon-item.component';
 import {HttpClientModule} from '@angular/common/http';
 // import {Ng2SearchPipeModule} from 'ng2-search-filter';
 import {FormsModule} from '@angular/forms';
-import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
-import { SearchFilterPipe } from './search-filter.pipe';
+import {PokemonDetailComponent} from './pokemon-detail/pokemon-detail.component';
+import {SearchFilterPipe} from './search-filter.pipe';
 
 // import { BackButtonDisableModule } from 'angular-disable-browser-back-button';
 
@@ -50,10 +50,16 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     //   preserveScrollPosition: true
     // })
   ],
-  providers: [ {
+  providers: [{
     provide: PERFECT_SCROLLBAR_CONFIG,
     useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
-  }],
+  },
+    {
+      provide: RouteReuseStrategy,
+      useClass: CustomRouteReuseStrategy
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
