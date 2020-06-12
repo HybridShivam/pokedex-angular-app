e5897957d80bdcd972c0a072eb83ca1acd4504af
Shivam Baghel <shivamunlimited@gmail.com>
2020-06-12 22:53:39 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,27 +1,27 @@
 ::Building
 echo Building >Build.log
-CALL ng build --prod --baseHref /pokedex --serviceWorker true >>Build.log
+CALL ng build --prod --baseHref /pokedex/ --serviceWorker true
 
 ::Clean Previous Build
 echo Cleaning >>Build.log
-rmdir /Q /S deploy\pokedex >>Build.log
-del /f deploy\pokedex.zip >>Build.log
+rmdir /Q /S deploy\pokedex 
+del /f deploy\pokedex.zip
 
 ::Copy from dist\ to deploy\
 echo Copying >>Build.log
-Xcopy /E dist deploy\ >>Build.log
+Xcopy /E dist deploy\
 
 ::Copy .htaccess to deploy\pokedex
 echo .htaccess >>Build.log
-copy deploy\.htaccess deploy\pokedex\ >>Build.log
+copy deploy\.htaccess deploy\pokedex\
 
 ::Use Brotli Compression on generated js,css,json,csv etc.
 echo Brotli >>Build.log
-CALL gulp compress >>Build.log
-cd deploy\ >>Build.log
+CALL gulp compress
+cd deploy\
 
 ::Use 7zip to create archive of deploy folder
 cd deploy
-echo Zipping >>Build.log
+echo Zipping >Build.log
 7z a pokedex.zip pokedex\ >>Build.log
 PAUSE
